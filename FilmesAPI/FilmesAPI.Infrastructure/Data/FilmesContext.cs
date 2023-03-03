5b0959b37021eaabe803178e2e1bc7191506530b
Júlio Santana <acsaoinbdevops@outlook.com>
2023-03-03 16:22:31 -0300

    Adicionando projeto de infraestrutura

---
+++ FilmesAPI/FilmesAPI.Infrastructure/Data/FilmesContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Infrastructure.Data
+{
+    public class FilmesContext : DbContext
+    {
+        public FilmesContext(DbContextOptions<FilmesContext> options) 
+            : base (options)
+        {
+
+        }
+
+
+    }
+}
